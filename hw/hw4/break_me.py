1cc01acbdd7dbffd3be613e6e7a101e903d0824f
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-17 13:24:03 -0700

    Added attribute error

--- hw/hw4/break_me.py
+++ hw/hw4/break_me.py
@@ -2,4 +2,12 @@ def exhibit_name_error():
     print(foo)
 
 def exhibit_type_error():
-    print(3 + "a")
+    print(3 / "a")
+
+def exhibit_attribute_error():
+    foobar = 10
+    print(foobar.upper())
+
+exhibit_name_error()
+exhibit_type_error()
+exhibit_attribute_error()
