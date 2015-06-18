4157fba3e169b4d840a931d7fd2c87039f3c9f0c
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-17 20:39:15 -0700

    Added syntax error

--- hw/hw4/break_me.py
+++ hw/hw4/break_me.py
@@ -8,6 +8,9 @@ def exhibit_attribute_error():
     foobar = 10
     print(foobar.upper())
 
+def exhibit_syntax_error()
+    print x
+
 exhibit_name_error()
 exhibit_type_error()
 exhibit_attribute_error()
