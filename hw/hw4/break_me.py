cf2f0f812bf60752384f575cb963b7953a37afdb
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-20 00:42:37 -0700

    Added comments

--- hw/hw4/break_me.py
+++ hw/hw4/break_me.py
@@ -1,16 +1,21 @@
 def exhibit_name_error():
+    # variable doesn't exist
     print(foo)
 
 def exhibit_type_error():
+    # you can't really divide a letter
     print(3 / "a")
 
 def exhibit_attribute_error():
+    #numbers aren't letters
     foobar = 10
     print(foobar.upper())
 
 def exhibit_syntax_error()
+everything is typed wrong
     print x
 
 exhibit_name_error()
 exhibit_type_error()
 exhibit_attribute_error()
+exibit syntax error
