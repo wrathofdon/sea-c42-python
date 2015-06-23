0e3395263ebfe22b00d10168b517327db1139ce4
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-22 17:22:26 -0700

    added lucas series

--- hw/session02/series.py
+++ hw/session02/series.py
@@ -12,11 +12,28 @@ def fibonacci(length):
             i += 1
             if (i > 1):
                 string.append(string[i - 1] + string[i - 2])
-        print(string[(length)])
+        return string[(length)]
 
 
-fibonacci(1)
-fibonacci(2)
-fibonacci(3)
-fibonacci(4)
-fibonacci(5)
+def lucas(length):
+    if (type(length) != int or length < 0):
+        print("Positive intergers only")
+        return False
+    elif (length == 0):
+        return 2
+    elif (length == 1):
+        return 1
+    else:
+        string = [2, 1]
+        for i in range(length):
+            i += 1
+            if (i > 1):
+                string.append(string[i - 1] + string[i - 2])
+        return string[(length)]
+
+
+lucas(1)
+lucas(2)
+lucas(3)
+lucas(4)
+lucas(5)
