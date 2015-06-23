86f71df57862e0ea154adcfdd9f10451614c4f1c
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-22 17:38:45 -0700

    added doc strings

--- hw/session02/series.py
+++ hw/session02/series.py
@@ -10,10 +10,15 @@ def fibonacci(length):
         """Addition is only necessary if the parameter is 2 or more"""
     else:
         string = [0, 1]
+        """The sequence is represented as a list"""
         for i in range(length):
             if (i > 0):
                 string.append(string[i] + string[i - 1])
+                """if addition is necessary, it only counts if the string is
+                more than 2.  At which point, you add the previous numbers on
+                list and append the new sum."""
         return string[(length)]
+        """Once range is completeled, we return the last number on list"""
 
 
 def lucas(length):
@@ -33,6 +38,7 @@ def lucas(length):
 
 
 def sum_series(length, x=0, y=1):
+    """Default starting numbers are 0 & 1 unless otherwise specified"""
     if (type(length) != int or length < 0):
         print("Positive intergers only")
         return False
@@ -52,3 +58,4 @@ sum_series(10)
 sum_series(10, 2, 1)
 sum_series(10, 4, 53)
 sum_series(10, 27, 4)
+"""This was just a test"""
