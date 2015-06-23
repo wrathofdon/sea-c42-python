630aba727c2344fa2d201e7214c33b790bdcd2a3
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-22 19:44:39 -0700

    Problem 5

--- hw/homework7/dna_analysis.py
+++ hw/homework7/dna_analysis.py
@@ -52,6 +52,10 @@ total_count = 0
 # Number of G and C nucleotides seen so far.
 gc_count = 0
 at_count = 0
+a_count = 0
+t_count = 0
+g_count = 0
+c_count = 0
 
 
 # for each base pair in the string,
@@ -60,18 +64,37 @@ for bp in seq:
     total_count = total_count + 1
 
     # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
+    # if bp == 'C' or bp == 'G':
         # increment the count of gc
-        gc_count = gc_count + 1
-    if bp == 'A' or bp == 'T':
-        # increment the count of gc
-        at_count = at_count + 1
+    #     gc_count = gc_count + 1
+    # if bp == 'A' or bp == 'T':
+        # increment the count of at
+    #    at_count = at_count + 1
+    if bp == 'A':
+        a_count += 1
+    if bp == 'T':
+        t_count += 1
+    if bp == 'G':
+        g_count += 1
+    if bp == 'C':
+        c_count += 1
 
 
 # divide the gc_count by the total_count
+gc_count = g_count + c_count
+at_count = a_count + t_count
+# total at & gc are calculated only once
 gc_content = float(gc_count) / total_count
 at_content = float(at_count) / total_count
+a_content = float(a_count) / total_count
+t_content = float(t_count) / total_count
+g_content = float(g_count) / total_count
+c_content = float(c_count) / total_count
 
 # Print the answer
 print('GC-content:', gc_content)
 print('AT-content:', at_content)
+print('A-content:', a_content)
+print('T-content:', t_content)
+print('G-content:', g_content)
+print('C-content:', c_content)
