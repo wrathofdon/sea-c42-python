4fbb7bd7e93e0f83d1625a6b5176770ff039ae27
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-22 17:27:21 -0700

    added series_sum

--- hw/session02/series.py
+++ hw/session02/series.py
@@ -37,3 +37,26 @@ lucas(2)
 lucas(3)
 lucas(4)
 lucas(5)
+
+
+def sum_series(length, x=0, y=1):
+    if (type(length) != int or length < 0):
+        print("Positive intergers only")
+        return False
+    elif (length == 0):
+        return x
+    elif (length == 1):
+        return y
+    else:
+        string = [x, y]
+        for i in range(length):
+            i += 1
+            if (i > 1):
+                string.append(string[i - 1] + string[i - 2])
+        return string[(length)]
+
+
+sum_series(10)
+sum_series(10, 2, 1)
+sum_series(10, 4, 53)
+sum_series(10, 27, 4)
