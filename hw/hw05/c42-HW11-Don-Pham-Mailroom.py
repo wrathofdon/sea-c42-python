396eb67122b7c45a792d7d1e59f48689da368aee
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-26 10:31:08 -0700

    Testing report functionality

--- hw/hw05/c42-HW11-Don-Pham-Mailroom.py
+++ hw/hw05/c42-HW11-Don-Pham-Mailroom.py
@@ -1,20 +1,24 @@
 # Dictionary sorting guide: http://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
 # Reverse order: http://stackoverflow.com/questions/5455606/how-to-reverse-order-of-keys-in-python-dict
 
+contacts = {
+    "Abe": [1, 2, 3, 4, 5],
+    "Bob": [6, 7, 8, 9, 10],
+    "Carl": [11, 12, 13, 14, 15]
+}
 
-contacts = {}
 
-
-def statup_boot():
+def startup_boot():
+    print("----------------------------")
     print("Welcome to Mailroom Madness")
     print("Choose from the following:")
     print("T - Send a (T)hank You and Print E-Mail")
     print("R - Create a (R)eport")
-    response = input("quit - Quit the program")
+    response = input("quit - Quit the program: ")
     response = response[:1]
     response = response.lower()
     if (response == "q"):
-        break
+        quit()
     elif (response == "t"):
         thank_you()
     elif (response == "r"):
@@ -25,19 +29,22 @@ def statup_boot():
 
 
 def thank_you():
+    print("----------------------------")
     print("Please enter a name, or choose from the following:")
     print("list - Print a list of previous donors")
-    donor = input("quit - Return to main menu.")
+    donor = input("quit - Return to main menu: ")
     if (donor.lower() == "q" or donor.lower() == "quit"):
         startup_boot()
     elif (donor.lower() == "l" or donor.lower() == "list"):
-        print_list()
+        print("----------------------------")
+        for donor in sorted(contacts):
+            print(donor)
         thank_you()
     else:
-        cash = int(input("Please enter a donation amount or 'quit':"))
+        cash = int(input("Please enter a donation amount or 'quit': "))
         if (cash.lower() == "q" or cash.lower() == "quit"):
             statup_boot()
-        elif (response in contacts):
+        elif (donor in contacts):
             history = contacts[donor]
             history.append(cash)
             print_letter()
@@ -46,11 +53,28 @@ def thank_you():
             print_letter()
 
 
+def print_list():
+    print("print_list")
+
+
+def print_letter():
+    print("print_letter")
+
+
 def generate_report():
     contacts_total = {}
     sorted_list = []
-    for key, value in contacts:
-        contacts_total.update({key: sum(value)})
-    for key, value in sorted(contacts_total.iteritems(), key=lambda (k,v): (v,k)):
-        storted_list.append(key)
+    for key in contacts:
+        contacts_total.update({key: sum(contacts[key])})
+    for item in sorted(contacts_total.items(), key=lambda x: x[1]):
+        sorted_list.append(item[0])
+    for name in sorted_list:
+        donations_total = contacts_total[name]
+        donations_number = len(contacts[name])
+        donations_average = donations_total / donations_number
+        print("----------------------------")
+        print(name, donations_total, donations_number, donations_average)
+    startup_boot()
 
+
+startup_boot()
