0559a8302abb8ab8d865fac58065168b6ddb3d33
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-06-27 00:19:31 -0700

    Fixed potential problem with sorting two names with same total

--- hw/hw05/c42-HW11-Don-Pham-Mailroom.py
+++ hw/hw05/c42-HW11-Don-Pham-Mailroom.py
@@ -44,24 +44,71 @@ def thank_you():
     elif (donor.lower() == "l" or donor.lower() == "list"):
         print("-" * 79)
         for name in sorted(contacts[0::3]):
+            # sorts & every third item in contacts list, which are the names
             print(name)
         thank_you()
     elif (donor in contacts[0::3]):
+        # checks to see if name is already in list
         print(donor, "selected")
-        # confirms that user made right choice
+        # confirms that user made right choice, then calls for cash
         process_cash(donor)
         startup_boot()
     else:
         contacts.extend([donor, [], 0])
         print("Adding %s to donation list (Pending)..." % donor)
-        # Lets user know that new entry was added
+        # Lets user know that new entry was added, then calls for cash
         process_cash(donor)
         startup_boot()
+
+
+def process_cash(donor):
+    """Checks the donation, gives user the chance to back out"""
+    index = contacts.index(donor)
+    # code searches for coordinates of donor for later use
+    cash = "cash"
+    # cash is declared as invalid from start, in order to launch promp
+    while (not cash.replace('.', '', 1).isdigit()):
+        # While loop activates if "cash" isn't an interget or decimal
+        cash = input("Please enter a donation amount or type 'undo':")
+        # user is given chance to change mind
+        if (cash.lower()[:1] == "q"):
+            quit()
+        if (cash.lower()[:1] == "u"):
+            if (contacts[index + 2] != 0):
+                pass
+                # old donor information is preserved if user changes mind
+            else:
+                del contacts[index:(index + 3)]
+                print(donor, "deleted from contacted list")
+                # removes new donor from contacts if user changes mind
+            thank_you()
+            # if user changes mind, they can now type a different donor
+        if (cash[:1] == "$"):
+            cash = cash[1:]
+            print(cash)
+            # removes "$" if "$" is used. We can re-test it in the next step
+        if (not cash.replace('.', '', 1).isdigit()):
+            print("Invalid Entry")
+            # Error message if the final entry is not a number
+    cash = float(cash)
+    # accepts payment in pennies
+    if ((cash * 100) % 1 != 0):
+        # rejects entry if there are too many decimal places, i.e., "$43.433"
+        print("Invalid Entry")
+        process_cash(donor)
+    else:
+        contacts[index + 1].append(cash)
+        contacts[index + 2] += cash
+        # adds cash donation to donor history & to the total, which are in the
+        # cells adjacent to the names
+        print_letter(donor, cash)
+        # calls print function, using donor and cash as arguments
 
 
 def print_letter(donor, cash):
     """Prints letter, using 'donor' and 'cash' as parameters"""
     cash = money_formatter(cash)
+    # calls for the money formmatter, which adds commas to large dollar amounts
     print("-" * 79)
     print("Constructing letter...")
     print("-" * 79)
@@ -74,45 +121,10 @@ def print_letter(donor, cash):
     input("Press Enter to continue...")
 
 
-def process_cash(donor):
-    """Checks the donation, gives user the chance to back out"""
-    cash = "cash"
-    index = contacts.index(donor)
-    while (not cash.replace('.', '', 1).isdigit()):
-        # While loop activates if "cash" isn't an interget or decimal
-        cash = input("Please enter a donation amount or type 'undo':")
-        if (cash.lower()[:1] == "q"):
-            quit()
-        if (cash.lower()[:1] == "u"):
-            if (contacts[index + 2] != 0):
-                pass
-            else:
-                del contacts[index:(index + 3)]
-                print(donor, "deleted from contacted list")
-                # removes donor from contacts if user changes mind
-            thank_you()
-        if (cash[:1] == "$"):
-            cash = cash[1:]
-            print(cash)
-            # removes "$" if "$" is used. We can re-test it in the next step
-        if (not cash.replace('.', '', 1).isdigit()):
-            print("Invalid Entry")
-            # Error message if the final entry is not a number
-    cash = float(cash)
-    if ((cash * 100) % 1 != 0):
-        # rejects entry if there are too many decimal places, i.e., "$43.433"
-        print("Invalid Entry")
-        process_cash(donor)
-    else:
-        contacts[index + 1].append(cash)
-        contacts[index + 2] += cash
-        # adds cash donation to donor history
-        print_letter(donor, cash)
-        # calls print function, using donor and cash as arguments
-
-
 def money_formatter(amount, remove=0):
     """Format dollar amount as a formatted string with whole dollars"""
+    # "remove" is optional parameter that cuts off the end of the final string.
+    # if set to -3, it will convert $X.YZ to $X, useful for final report.
     if (type(amount) == str):
         return spaces_formatter(16, amount)
         # This is so I can use a regular string in the title bar
@@ -126,6 +138,7 @@ def money_formatter(amount, remove=0):
         amount = amount[:-14] + "," + amount[-14:]
         # adds commas when appropriate
     amount = amount[0:(len(amount) + remove)]
+    # "remove" is optional parameter to remove pennies from dollar amounts
     return amount
 
 
@@ -140,44 +153,54 @@ def spaces_formatter(total, string):
     return string
 
 
-def report_line(name, total, number, average):
+def report_line(name, total, number, average, remove=0):
     """Takes raw inputs, calls on the formatters, and generates a line"""
     name = spaces_formatter(32, name)
-    total = spaces_formatter(16, money_formatter(total, -3))
+    total = spaces_formatter(16, money_formatter(total, remove))
     number = spaces_formatter(6, number)
-    average = spaces_formatter(16, money_formatter(average, -3))
+    average = spaces_formatter(16, money_formatter(average, remove))
     print(name, "|", total, "|", number, "|", average)
 
 
 def sort_contacts(contacts, number_of_names):
+    """Sorts contacts list by total donation value"""
     for i in range(number_of_names):
+        # number of names = len(contacts) / 3,
         i = i * 3 + 1
+        # This starts at 1 and counts by 3, provides the donation history lists
         contacts[i + 1] = sum(contacts[i])
+        # Takes the sum of each list and places the total in the next element
+    temp_contacts = list(contacts)
+    # we need to create a temporary copy of the contacts list
     sorted_amounts = (sorted(contacts[2::3]))
+    # this pulls the total values from the list and sorts them
     temp_list = []
     for amount in sorted_amounts:
-        index = contacts.index(amount) - 2
-        for i in range(3):
-            temp_list.append(contacts[index + i])
+        index = temp_contacts.index(amount) -2
+        temp_list.extend(temp_contacts[index:(index + 3)])
+        del temp_contacts[index:(index + 3)]
     return temp_list
 
 
 def generate_report():
-    number_of_names = int(len(contacts) / 3)
     """Generates report of past donations"""
+    number_of_names = int(len(contacts) / 3)
+    # counts the number of names in contacts list
     print("-" * 79)
-    report_line("Name :", "$Total :  ", "# :", "$Average :  ")
+    report_line("Name:", "$Total:  ", "#:", "$Average:  ")
     # Prints out the title bar
+    # Spaces are added to offset remove in the report_line
     print("-" * 79)
-    global contacts
-    contacts = sort_contacts(contacts, number_of_names)
+    # prevents shadow variable
+    temp_contacts = sort_contacts(contacts, number_of_names)
     for i in range(number_of_names):
-        name = contacts[i * 3]
-        total = contacts[i * 3 + 2]
-        number = len(contacts[i * 3 + 1])
+        name = temp_contacts[i * 3]
+        total = temp_contacts[i * 3 + 2]
+        number = len(temp_contacts[i * 3 + 1])
         average = total / number
-        report_line(name, total, number, average)
+        report_line(name, total, number, average, -3)
         # for each name in the list, we generate a line
+        # this is the only time the "remove" parameter is called
     startup_boot()
 
 
