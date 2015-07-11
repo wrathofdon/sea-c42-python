218ad1617ed9a9b11735b4daba3dc5b9e5fffdad
DON B. PHAM <WRATHOFDON@GMAIL.COM>
2015-07-11 13:19:36 -0700

    Exercise 4

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -9,78 +9,80 @@ Python class example.
 # Fill it all in here.
 
 
-IND_LVL = "    "
-
+TAB = "    "
 
 class Element(object):
 
-    def __init__(self, tag="", content="", indent=0):
-        self.tag = tag
+    def __init__(self, tag="", content="", indent=0, kwargs={}):
+        self.tag1 = tag
+        self.tag2 = tag
+        for name, value in kwargs.items():
+            self.tag1 = self.tag1 + ' %s="%s"' % (name, value)
+            print(name, value)
         self.children = [content] if content else []
-        self.indent = IND_LVL * indent
-        print(self.tag)
+        self.indent = TAB * indent
 
     def append(self, new_child):
         self.children.append(new_child)
 
     def render(self, file_out):
-        file_out.write("\n%s<%s>" % (self.indent, self.tag))
+        file_out.write("\n%s<%s>" % (self.indent, self.tag1))
         for child in self.children:
             if(type(child) == str):
-                file_out.write("\n" + self.indent + IND_LVL + child)
+                file_out.write("\n" + self.indent + TAB + child)
             else:
                 child.render(file_out)
-        file_out.write("\n%s</%s>" % (self.indent, self.tag))
+        file_out.write("\n%s</%s>" % (self.indent, self.tag2))
 
 
 class Html(Element):
 
-    def __init__(self, tag="", content="", indent=0):
-        Element.__init__(self, "html", "", 0)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, "html", content, 0, kwargs)
 
     def render(self, file_out):
         file_out.write("<!DOCTYPE html>")
-        file_out.write("\n%s<%s>" % (self.indent, self.tag))
+        file_out.write("\n%s<%s>" % (self.indent, self.tag1))
         for child in self.children:
             if(type(child) == str):
-                file_out.write("\n" + self.indent + IND_LVL + child)
+                file_out.write("\n" + self.indent + TAB + child)
             else:
                 child.render(file_out)
-        file_out.write("\n%s</%s>" % (self.indent, self.tag))
+        file_out.write("\n%s</%s>" % (self.indent, self.tag2))
 
 
 class Body(Element):
 
-    def __init__(self, tag="", content="", indent=0):
-        Element.__init__(self, "body", "", 1)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, "body", content, 1, kwargs)
 
 
 class P(Element):
 
-    def __init__(self, content="", indent=0):
-        Element.__init__(self, "p", content, 2)
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, "p", content, 2, kwargs)
 
 
 class Head(Element):
 
-    def __init__(self, content="", indent=0):
-        Element.__init__(self, "head", content, 1)
-        print("Head")
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, "head", content, 1, kwargs)
 
 
 class Title(Element):
 
-    def __init__(self, content="", indent=0):
-        Element.__init__(self, "title", content, 2)
-        print("Title")
+    def __init__(self, content="", **kwargs):
+        Element.__init__(self, "title", content, 2, kwargs)
 
     def render(self, file_out):
-        file_out.write("\n%s<%s>%s</%s>" % (self.indent, self.tag, \
-            self.children[0], self.tag))
+        file_out.write("\n%s<%s>%s</%s>" % (self.indent, self.tag1, \
+            self.children[0], self.tag2))
 
 
 
 """
+
+
 page = hr.Html()
 
 head = hr.Head()
@@ -90,10 +92,10 @@ page.append(head)
 
 body = hr.Body()
 
-body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text"))
-body.append(hr.P(u"And here is another piece of text -- you should be able to add any number"))
+body.append(hr.P(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text",
+              style=u"text-align: center; font-style: oblique;"))
 
 page.append(body)
 
-render(page, u"test_html_output3.html")
+render(page, u"test_html_output4.html")
 """
